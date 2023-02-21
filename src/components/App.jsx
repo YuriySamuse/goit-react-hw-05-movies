cdd0d8350a698349922c536f9b7a8291903951a7
YuriySamusew <6177385@gmail.com>
2023-02-21 23:43:56 +0200

    Routers - URL-params

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from 'pages/Home';
+import About from 'pages/About';
+import Products from 'pages/Products';
+import ProductDetails from 'pages/ProductDetails';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
+  const ProductDetails = () => {
+    const { productId } = useParams();
+    return <div>Now showing product with id - {productId}</div>;
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 };
