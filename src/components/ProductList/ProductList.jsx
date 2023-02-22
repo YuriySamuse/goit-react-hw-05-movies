2513196939ccb71c765ac1669f4d3e27345a426f
YuriySamusew <6177385@gmail.com>
2023-02-22 02:53:12 +0200

    recommit after lint

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  CardWrapper,
+  ProductName,
+} from 'components/ProductList/ProductList.styled';
+
+export const ProductList = ({ products }) => {
+  return (
+    <Container>
+      {products.map(product => (
+        <CardWrapper key={product.id}>
+          <Link to={`${product.id}`}>
+            <img src="https://via.placeholder.com/200x100" alt="" />
+            <ProductName>{product.name}</ProductName>
+          </Link>
+        </CardWrapper>
+      ))}
+    </Container>
+  );
+};
